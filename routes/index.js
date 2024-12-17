2b37994d1ac771ed0a6edf6c18e97b501467e43b
Girgetto <girgetto@users.noreply.github.com>
2024-12-17 10:26:37 +0100

    Add profile route, implement email display, and enhance helper functions

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,16 @@ const isAuthenticated = require('../middlewares/isAuthenticated');
 const isAdmin = require('../middlewares/isAdmin');
 
 router.use('/auth', require('./auth'));
-
 router.use('/admin', isAdmin, require('./admin'));
 
-router.get('/', isAuthenticated, function(req, res) {
+router.get('/profile', (req, res) => {
+  const { email } = req.user;
+
+  res.render('profile', { email })
+})
+
+router.get('/', isAuthenticated, function (req, res) {
+  console.log(req.session)
   res.render('index', { title: 'Express project template' });
 });
 
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,3 @@
-module.exports = {};
+module.exports = {
+  toUpperCase: (string) => string.toUpperCase(),
+};
